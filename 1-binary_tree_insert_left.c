05dd4039a0b87848ee4e45780f8d8e1881999a2d
Rain Nyte <rain_nyte@proton.me>
2024-04-19 18:38:16 -0500

    added handling for if the parent node already has a left child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,11 +18,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *lotus, int value)
 	}
 	else
 	{
-		ten0->n = value;
-		ten0->parent = lotus;
-		ten0->right = NULL;
-		ten0->left = NULL;
-		lotus->left = ten0;
-		return (ten0);
+		if (lotus->left != NULL)
+		{
+			binary_tree_insert_left((lotus->left), value);
+		}
+		else
+		{
+			ten0->n = value;
+			ten0->parent = lotus;
+			ten0->right = NULL;
+			ten0->left = NULL;
+			lotus->left = ten0;
+			return (ten0);
+		}
 	}
 }
