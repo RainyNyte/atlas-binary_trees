a2c0ce4dae49480ff8a0cb71d550c5797fc99729
Rain Nyte <rain_nyte@proton.me>
2024-04-19 22:46:51 -0500

    fixed typo in binary_tree_is_full function

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,7 +7,7 @@
  * Return: 1 if the tree is full, 0 otherwise
 */
 
-int binary_tree_is_full(const binarry_tree_t *root)
+int binary_tree_is_full(const binary_tree_t *root)
 {
 	if (root == NULL)
 		return (0);
