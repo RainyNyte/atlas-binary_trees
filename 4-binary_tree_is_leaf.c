f531a280a8d54847384fe2bfb918f8bfe495af7a
Rain Nyte <rain_nyte@proton.me>
2024-04-19 19:32:54 -0500

    made 4-binary_tree_is_leaf.c betty compliant

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_is_leaf- checks to see if a given node in a tree is a leaf
  * @node: the node to check
- * 
+ *
  * Return: 1 if the node is a leaf or 0 otherwise
 */
 
@@ -13,5 +13,5 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	{
 		return (0);
 	}
-	return (node->left == NULL && node->right == NULL) ? 1 : 0;
+	return ((node->left == NULL && node->right == NULL) ? 1 : 0);
 }
