86a63ace01c2410e66d49c3ee2e784972f201a6a
Rain Nyte <rain_nyte@proton.me>
2024-04-19 20:30:30 -0500

    fixed handling for if the tree is NULL

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,14 @@ size_t binary_tree_height(const binary_tree_t *root)
 	size_t height_l;
 	size_t height_r;
 
-	height_l = root->left ? 1 + binary_tree_height(root->left) : 0;
-	height_r = root->right ? 1 + binary_tree_height(root->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+	if (root == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		height_l = root->left ? 1 + binary_tree_height(root->left) : 0;
+		height_r = root->right ? 1 + binary_tree_height(root->right) : 0;
+		return (height_l > height_r ? height_l : height_r);
+	}
 }
