66a9d46bc01b5efbe957122e40b2d81ccb70a4b2
Rain Nyte <rain_nyte@proton.me>
2024-04-19 22:20:42 -0500

    divided height helper function into two helpers and changed final calculation of height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,27 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height- measures the height of a binary tree
- * @root: pointer to the root node of the tree to measure
+ * max- helper function for height
+ * calculates the maximum value between two ints
+ * @a: first int to check
+ * @b: second int to check
  *
- * Return: height of the tree or 0 if root is NULL
+ * Return: value of the larger int
 */
 
-size_t binary_tree_height(const binary_tree_t *root)
+int max(int a, int b)
 {
-	size_t height_l;
-	size_t height_r;
+	return ((a > b) ? a : b);
+}
 
-	if (root == NULL)
-	{
+/**
+ * height- helper function for binary_tree_balance
+ * calculates the height of a tree
+ * @root: pointer to the root of the tree to measure
+ *
+ * Return: height of the tree
+*/
+
+int height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
 		return (0);
-	}
-	else
-	{
-		height_l = root->left ? 1 + binary_tree_height(root->left) : 0;
-		height_r = root->right ? 1 + binary_tree_height(root->right) : 0;
-		return (height_l > height_r ? height_l : height_r);
-	}
+
+	int left_height = height(tree->left);
+	int right_height = height(tree->right);
+	return (1 + max(left_height, right_height));
 }
 
 /**
@@ -35,8 +43,8 @@ size_t binary_tree_height(const binary_tree_t *root)
 
 int binary_tree_balance(const binary_tree_t *root)
 {
-	int left_height = binary_tree_height(root->left);
-	int right_height = binary_tree_height(root->right);
+	int left_height = height(root->left);
+	int right_height = height(root->right);
 
 	if (root == NULL)
 		return (0);
