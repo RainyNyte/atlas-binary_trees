5e35975ba89da1532fed50a90613613a3e338623
Rain Nyte <rain_nyte@proton.me>
2024-04-19 20:20:10 -0500

    betty compliance

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -10,7 +10,7 @@ void binary_tree_postorder(const binary_tree_t *root, void (*func)(int))
 {
 	if (root == NULL || func == NULL)
 		return;
-	
+
 	binary_tree_postorder(root->left, func);
 	binary_tree_postorder(root->right, func);
 	func(root->n);
