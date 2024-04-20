957f9ad4f903de6cd34bed131998bf08846d8c14
Rain Nyte <rain_nyte@proton.me>
2024-04-19 20:19:15 -0500

    implemented function to navigate tree using  postorder traversal

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder- navigates a tree using postorder traversal
+ * @root: pointer to root node of the tree
+ * @func: pointer to function to be called on each node of the tree
+*/
+
+void binary_tree_postorder(const binary_tree_t *root, void (*func)(int))
+{
+	if (root == NULL || func == NULL)
+		return;
+	
+	binary_tree_postorder(root->left, func);
+	binary_tree_postorder(root->right, func);
+	func(root->n);
+}
