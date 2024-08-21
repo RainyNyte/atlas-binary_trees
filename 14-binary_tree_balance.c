85ce6dae1a0045bab0128926d96adc0d461b2477
Rain Nyte <rain_nyte@proton.me>
2024-08-21 17:25:45 -0500

    fixed mixed declaration

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,11 +8,14 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height;
+	int right_height;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	int left_height = tree->left ? (int)binary_tree_height(tree->left) : 0;
-	int right_height = tree->right ? (int)binary_tree_height(tree->right) : 0;
+	left_height = tree->left ? (int)binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? (int)binary_tree_height(tree->right) : 0;
 	return (left_height - right_height);
 }
